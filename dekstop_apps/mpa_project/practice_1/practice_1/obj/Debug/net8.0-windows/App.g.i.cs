295e2098c2fe82f53c39472836581d4ef2b40f20
StormJuly0303 <stormjuly@outlook.com>
2024-02-19 10:36:11 -0600

    ddd

--- dekstop_apps/mpa_project/practice_1/practice_1/obj/Debug/net8.0-windows/App.g.i.cs
+++ dekstop_apps/mpa_project/practice_1/practice_1/obj/Debug/net8.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace practice_1 {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace practice_1 {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.2.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.0.0")]
         public static void Main() {
             practice_1.App app = new practice_1.App();
             app.InitializeComponent();
--- dekstop_apps/mpa_project/practice_1/practice_1/obj/Debug/net8.0-windows/practice_1.AssemblyInfo.cs
+++ dekstop_apps/mpa_project/practice_1/practice_1/obj/Debug/net8.0-windows/practice_1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("practice_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfa9a53b9198df5411de08db3d4fa992b1d0f6c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("practice_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("practice_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
