632eb0b9c5b03504fea5f2b7cbc9ffd9efa0efcd
Storm July <stormjuly@outlook.com>
2024-02-21 00:45:45 -0600

    glih

--- practice/wpf/signup_form/signup_form/MainWindow.xaml.cs
+++ practice/wpf/signup_form/signup_form/MainWindow.xaml.cs
@@ -11,14 +11,35 @@ using System.Windows.Shapes;
 
 namespace signup_form
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    
     public partial class MainWindow : Window
     {
+        bool running = false;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+        
+        
+        private void btnToggleRun_Click(object sender, RoutedEventArgs e)
+        {
+            if(running)
+            {
+                tbStatus.Text = "Stopped";
+                btnToggleRun.Content = "Run";
+            }
+
+            else
+            {
+                tbStatus.Text = "Running";
+                btnToggleRun.Content = "Stop";
+            }
+
+            running = !running;
+
+        }
+
+
     }
 }
